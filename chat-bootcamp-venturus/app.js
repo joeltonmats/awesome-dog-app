f13316dd39babe044233b4c5e73d06e4a512bfe2
Matosjoe <joelton.matos@lhs-systems.com>
2017-12-06 13:38:06 -0200

    changed

--- chat-bootcamp-venturus/app.js
+++ chat-bootcamp-venturus/app.js
@@ -10,7 +10,7 @@ app.get('*', (req, res) => {
     res.sendfile(path.join(__dirname, 'dist/index.html'));
 });
 
-const port = process.env.Port || '3001';
+const port = process.env.PORT || '3001';
 app.set('port', port);
 
 const server = http.createServer(app);
